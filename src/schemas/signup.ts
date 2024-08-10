dcd40660e7227d281505cf908e13f7c277d243c6
Anas <anasdaud1996@gmail.com>
2024-08-10 09:19:34 +0800

    Add Validation Schemas

---
+++ src/schemas/signup.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+const signupSchema = z.object({
+    name: z
+        .string()
+        .min(5, {
+            message: 'At least 5 characters required',
+        })
+        .max(20, {
+            message: 'Maximum length is 20 characters',
+        })
+        .trim(),
+    email: z.string().email().toLowerCase().trim(),
+    password: z
+        .string()
+        .min(5, {
+            message: 'At least 5 characters required',
+        })
+        .max(5000, {
+            message: 'Maximum length is 5000 characters',
+        }),
+})
+
+export default signupSchema
